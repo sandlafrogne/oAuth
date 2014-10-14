670e37a82b1d205f9dba0de4cf069776da891bac
unknown <TNVL6480@EB-GG9QV4J.ad.francetelecom.fr>
2014-10-14 12:54:45 +0200

    oauth

--- client/js/app.js
+++ client/js/app.js
@@ -1,68 +1,9 @@
 //TODO : changer le nom du module, même valeur que dans index.html
-angular.module('plunker', ['ngRoute', 'controllers', 'services', 'directives', 'filters', 'pascalprecht.translate','ngAnimate'])
-    .config(function ($routeProvider,$httpProvider) {
-        $routeProvider.when('/', {templateUrl: 'albums.html'})
-            .when('/pane', {templateUrl: 'pane.html'})
-            .when('/album/:id', {templateUrl: 'album.html'})
-            .when('/as', {templateUrl: 'as.html'})
-            .when('/albums', {templateUrl: 'albums.html'})
-            .when('/error', {templateUrl: 'error.html'})
-            .otherwise({
-                redirectTo: '/error'
-            });
-        //l'interceptor permet de mettre le nom de domaine devant l'url relative (accès à un serveur sur un autre domaine)
-        $httpProvider.interceptors.push(function ($q, $window, $location, sharedProperties) {
-            return {
-                request: function (config) {
-                  /*  patt = new RegExp('out_rpc');
-                    patt2 = new RegExp('http://');
-                    if (patt.test(config.url) && !patt2.test(config.url)) {
-                        config.url = sharedProperties.getOutRPCUrl() + "/" + config.url;
-                    }
-                    config.headers = config.headers || {};
-                    if ($window.sessionStorage.ext_accessToken) {
-                        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.ext_accessToken;
-                    } else if ($window.localStorage.ext_accessToken) {
-                        $window.sessionStorage.ext_accessToken = $window.localStorage.ext_accessToken;
-                        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.ext_accessToken;
-                    }*/
-                    return config;
-                },
-                'responseError': function (rejection) {
-                    if (rejection.status === 401) {
-                        $location.path('/login');
-                    }
-                    return $q.reject(rejection);
-                }
-            }
-        });
-    })
-    .config(['$translateProvider', function ($translateProvider) {
-        $translateProvider.translations('en', {
-            'MY_ACCOUNT': 'My account',
-            'LOG_OUT': 'Log out',
-            'SEARCH': 'Search',
-            'ERROR': 'Error',
-            'AN_ERROR_HAS_OCCURRED': 'An error has occurred',
-            'PANE': 'Panes',
-            'HOME': 'home',
-            'AS': 'asController'
-        });
-        $translateProvider.translations('fr', {
-            'MY_ACCOUNT': 'Mon compte',
-            'LOG_OUT': 'Se déconnecter',
-            'SEARCH': 'Rechercher',
-            'ERROR': 'Erreur',
-            'AN_ERROR_HAS_OCCURRED': 'Une erreur s\'est produite',
-            'PANE': 'Onglets',
-            'HOME': 'accueil',
-            'AS': 'Controlleur avec As'
-        });
-        $translateProvider.preferredLanguage('fr');
-    }])
+
+//twitterApp is dependent on the twitterApp.services module
+var app = angular.module('twitterApp', ['twitterApp.services']);
 
 
-/*Ajout des modules externes*/
 
 angular.module('controllers', []);
 angular.module('services', []);
--- client/js/controllers/mainCtrl.js
+++ client/js/controllers/mainCtrl.js
@@ -1,149 +1,44 @@
-/* Contrôleur principal
- ================================================== */
+///inject the twitterService into the controller
+app.controller('TwitterController', function($scope, $q, twitterService) {
 
+    $scope.tweets; //array of tweets
 
-/* Contrôleur principal
- ================================================== */
-angular.module('controllers', [])
-    .controller("MainCtrl", ["$scope", "$routeParams",
-        function ($scope, $routeParams) {
-            $scope.view = false;
-            DZ.init({
-                appId: '122865',
-                channelUrl: 'http://external.codecademy.com/channel.html',
-                player: {
-                    onload: function () {
-                    }
-                }
-            })
+    twitterService.initialize();
 
-            albumId = $routeParams.id
-            //console.log('albumId : ' + albumId)
-            DZ.api('/album/' + albumId, function (json) {
-                console.log(json)
-                $scope.cover = json.cover;
-                $scope.tracks = json.tracks.data;
-                //console.log(json.tracks.data)
-                var tracks_ids = [];
-                for (var prop in $scope.tracks) {
-                    tracks_ids.push($scope.tracks[prop].id);
-                }
-                $scope.tracks_ids = tracks_ids;
-                $scope.view = true;
-                $scope.$apply();
-            });
-            $('#info .modal-body').html('liste récupérée')
-            $('#info').modal("show")
+    //using the OAuth authorization result get the latest 20 tweets from twitter for the user
+    $scope.refreshTimeline = function() {
+        twitterService.getLatestTweets().then(function(data) {
+            $scope.tweets = data;
+        });
+    }
 
-            $scope.play_track = function (index) {
-                DZ.player.playTracks($scope.tracks_ids, index, function (response) {
-                    console.log(LOGNS, "track list", response.tracks);
-                })
-            };
-
-            $scope.currentIndex = 0;
-
-            $scope.setCurrentSlideIndex = function (index) {
-                $scope.currentIndex = index;
-            };
-
-            $scope.isCurrentSlideIndex = function (index) {
-                return $scope.currentIndex === index;
-            };
-
-            $scope.prevSlide = function () {
-                $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-            };
-
-            $scope.nextSlide = function () {
-                $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-            };
-        }
-    ])
-
-    .controller("albumsController", ["$scope",
-        function ($scope) {
-            $scope.view = false;
-            DZ.init({
-                appId: '122865',
-                channelUrl: 'http://external.codecademy.com/channel.html',
-                player: {
-                    onload: function () {
-                    }
-                }
-            })
-            /*recherche des albums de Thomas Fersen*/
-            DZ.api('/artist/853/albums', function (json) {
-                $scope.albums = json.data;
-                $scope.$apply();
-            })
-            /* Affichage des covers en   slide*/
-            $scope.currentIndex = 0;
-            $scope.direction='left';
-
-
-            $scope.isCurrentSlideIndex = function (index) {
-                return $scope.currentIndex === index;
-            };
-            $scope.prevSlide = function () {
-                $scope.direction = 'left';
-                $scope.currentIndex = ($scope.currentIndex < $scope.albums.length - 1) ? ++$scope.currentIndex : 0;
-            };
-            $scope.nextSlide = function () {
-                $scope.direction = 'right';
-                $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.albums.length - 1;
-            };
-            $scope.showPhoto = function (index) {
-                $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-                $scope.currentIndex = index;
-            };
-        }])
-
-
-    .animation('.slide-animation2', function () {
-        return {
-            beforeAddClass: function (element, className, done) {
-                var scope = element.scope();
-                console.log('elt de before' + element)
-                if (className == 'ng-hide') {
-                    var finishPoint = element.parent().parent().parent().width();
-                    if(scope.direction !== 'right') {
-                        finishPoint = -finishPoint;
-                    }
-                    TweenMax.to(element, 2, {left: finishPoint, onComplete: done });
-                    //rétrécissement de l'image qui part
-                    TweenMax.staggerTo(element, 1,  {scale:0.2, opacity:0.3}, 0.25);
-                }
-                else {
-                    done();
-                }
-            },
-
-           /* beforeRemoveClass : function(element, className, done) {
-                var scope = element.scope();
-                TweenMax.staggerTo(element, 0,  {scale:1, opacity:1}, 0.25);
-            },*/
-            removeClass: function (element, className, done) {
-                var scope = element.scope();
-                console.log('elt de remove'+element)
-                if (className == 'ng-hide') {
-
-                    element.removeClass('ng-hide');
-                    var startPoint = element.parent().parent().parent().width();
-                    if(scope.direction === 'right') {
-                        startPoint = -startPoint;
-                    }
-                    /*entrée de l'image par la droite ou la gauche, suivant si c'est un
-                     clic flèche gauche ou droite ou dans la timeline si c'est un album précédent ou suivant*/
-                    TweenMax.staggerTo(element, 0,  {scale:0.2, opacity:0.3}, 0.25);
-                    TweenMax.fromTo(element, 4, { left: startPoint }, {left: 0, onComplete: done });
-                    //agrandissement de l'image qui arrive si elle a été réduite précédemment
-                    TweenMax.staggerTo(element, 1,  {scale:1, opacity:1}, 0.25);
-                }
-                else {
-
-                    done();
-                }
+    //when the user clicks the connect twitter button, the popup authorization window opens
+    $scope.connectButton = function() {
+        twitterService.connectTwitter().then(function() {
+            if (twitterService.isReady()) {
+                //if the authorization is successful, hide the connect button and display the tweets
+                $('#connectButton').fadeOut(function(){
+                    $('#getTimelineButton, #signOut').fadeIn();
+                    $scope.refreshTimeline();
+                });
             }
-        };
-    })
+        });
+    }
+
+    //sign out clears the OAuth cache, the user will have to reauthenticate when returning
+    $scope.signOut = function() {
+        twitterService.clearCache();
+        $scope.tweets.length = 0;
+        $('#getTimelineButton, #signOut').fadeOut(function(){
+            $('#connectButton').fadeIn();
+        });
+    }
+
+    //if the user is a returning user, hide the sign in button and display the tweets
+    if (twitterService.isReady()) {
+        $('#connectButton').hide();
+        $('#getTimelineButton, #signOut').show();
+        $scope.refreshTimeline();
+    }
+
+});
